6ec69c02c5e666cd2526590f00a60641d223db57
Yourinthemood <Darin.ulziibayar@ucas-edu.net>
2025-08-21 10:09:50 -0600

    seasons

--- practices/hello_world.py
+++ practices/hello_world.py
@@ -8,4 +8,10 @@ if name == a or name == b:
  print("hello admin", name)
 else:
  print("hello",name)
- 
+
+print("your family has been captured",name)
+name = input("if you want to see them again please enter your SSN or else: ")
+name = name
+print("haha, real funny, thats not a real SSN",name)
+print("say good bye now")
+print("with a sudden scream, came a loud bang and scilence.")
---
+++ starters/seasons.py
@@ -0,0 +1,5 @@
+# DU 2nd seasons 
+
+name = input("what is your last name: ")
+season=input("whats your favorite season: ")
+print("wow", name, "san, i like", season,"too!")
