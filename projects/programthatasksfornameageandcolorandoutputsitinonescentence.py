98aba3aed3a2c36ae93d570520d11321627c62d8
Yourinthemood <Darin.ulziibayar@ucas-edu.net>
2025-08-22 10:50:51 -0600

    yyyy

--- projects/programthatasksfornameageandcolorandoutputsitinonescentence.py
+++ projects/programthatasksfornameageandcolorandoutputsitinonescentence.py
@@ -1,1 +1,15 @@
 #DU 2nd Project
+print("hello friend!")
+
+import time 
+time.sleep(2)
+
+name = input("what is your name: ")
+print("hi",name)
+
+import time 
+time.sleep(1)
+
+age = input("what is your age?: ")
+
+
