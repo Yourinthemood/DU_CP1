cf8f84ed31a87ec76e542b3b88acd604f40ca91a
Yourinthemood <Darin.ulziibayar@ucas-edu.net>
2025-08-28 10:51:27 -0600

    hw

---
+++ notes_1st/numbers.py
@@ -0,0 +1,19 @@
+#DU 2nd integers and floats notes
+
+#integer
+age = 6742069
+temp = 85
+attention = -8
+spoons = 4
+#integer is a whole number
+emotional_damage = .99
+patience_level = .50
+pi = 3.14
+#float a number that has a decimal
+#(floating point number)
+#addition + subratcion - exponets ** multiplication * division / modules % integer division //
+
+#MATH ! ! ! 
+
+age = int(input("how old are you?\n"))
+print("wow that is old! that is", 5245600*age)
