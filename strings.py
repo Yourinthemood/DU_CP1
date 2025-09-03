0b14f1bdeb7373e376ef28842910f60f8b3bd494
yourinthemod <darin.ulziibayar@ucas-edu.net>
2025-09-03 15:52:01 -0600

    madlib

--- strings.py
+++ strings.py
@@ -1,1 +1,29 @@
 #DU 2nd strings notes
+
+#"contents"
+#'contents'
+
+#example
+
+str("the quick brown fox jumps over the lazy dog")
+
+first_name = 'xavaer'
+
+# if i need to use ' or "
+
+name = "las'rose"
+
+month = 'september' 
+
+# yoi dudes the empire is pretty chill, u should like join it or sumthin
+
+len(str("what is the biggest amount of the things that the things do"))
+
+length = len(month)
+print("september has", length, "letters")
+
+# escape character = /
+
+print('will said \n"don\'t die "')
+
+print()
