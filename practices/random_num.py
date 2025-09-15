cf28fb6b6f061cc33db33b2f2b943ec2d9bc610d
yourinthemod <darin.ulziibayar@ucas-edu.net>
2025-09-15 10:25:46 -0600

    hi

--- practices/random_num.py
+++ practices/random_num.py
@@ -1,60 +1,18 @@
-import random
+money = float(input("enter how much money the pirates earned:" ))
+priates_count = int(input("how many pirates are there:" ))
 
+captain_divide = 7
 
-print(random.randint(67, 67))
- 
+first_mate_divide = 3
 
-print("this is called the mason 67 game")
+crews_share = 1
 
-print("Press Enter to start the game")
-    
+total_shares = captain_divide + first_mate_divide + (crews_share * (priates_count - 2))
+share_value = money / total_shares  
+captain_money = share_value * captain_divide
+first_mate_money = share_value * first_mate_divide
+crew_money = share_value * crews_share + 500 - 1000
+print("captain gets:", captain_money)
+print("first mate gets:", first_mate_money)
+print("each crew still needs:", crew_money)
 
-print("you have 3 tries to guess the number"   
-      " between 1 and 100")
-
-number = random.randint(1, 100)
-
-tries = 0
-while tries < 3:
-
-    guess = int(input("Enter your guess: "))
-    tries += 1
-
-    if guess < number:
-        print("Too low!")
-    elif guess > number:
-        print("Too high!")
-    else:
-        print("Congratulations! You've guessed the number!")
-        break
-
-while tries == 3:
-    print(f"Sorry, you've used all your tries. The number was {number}.")
-    break   
-print("Thank you for playing the Mason 67 game!")
-
-while True:
-    play_again = input("Do you want to play again? (yes/no): ").lower()
-    if play_again == "yes":
-        number = random.randint(1, 100)
-        tries = 0
-        print("New game started! You have 3 tries to guess the number between 1 and 100.")
-        while tries < 3:
-            guess = int(input("Enter your guess: "))
-            tries += 1
-
-            if guess < number:
-                print("Too low!")
-            elif guess > number:
-                print("Too high!")
-            else:
-                print("Congratulations! You've guessed the number!")
-                break
-        if tries == 3:
-            print(f"Sorry, you've used all your tries. The number was {number}.")
-        print("Thank you for playing the Mason 67 game!")
-    elif play_again == "no":
-        print("Thank you for playing! Goodbye!")
-        break
-    else:
-        print("Invalid input. Please enter 'yes' or 'no'.")
