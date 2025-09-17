0f1d65522dbf23a405e5830b0be01553daa487f3
Yourinthemood <Darin.ulziibayar@ucas-edu.net>
2025-09-17 10:48:23 -0600

    hgi

---
+++ practices/hi.py
@@ -0,0 +1,4 @@
+#i need to create a very complicated way of printing user nputs in a cool way if i do this and it does that then i t wqill do whati want to do
+name = input("enter your name: ")
+age = input("enter your age: ") 
+
